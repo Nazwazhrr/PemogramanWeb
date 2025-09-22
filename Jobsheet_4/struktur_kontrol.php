5457b201d3dd0088d5c0668ad6672d21cdff3fe9
nazwazhrr <nazwaazahra883@gmail.com>
2025-09-22 21:34:58 +0700

    PHP

---
+++ Jobsheet_4/struktur_kontrol.php
@@ -0,0 +1,27 @@
+<?php
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+    echo "Nilai Huruf: A";
+} else if ($nilaiNumerik >= 80 && $nilaiNumerik <= 90) {
+    echo "Nilai Huruf: B";
+} else if ($nilaiNumerik >= 70 && $nilaiNumerik <= 80) {
+    echo "Nilai Huruf: C";
+} else if ($nilaiNumerik >= 70) {
+    echo "Nilai huruf: D ";
+}  
+echo "<br>";
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "<br>";
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+?>
