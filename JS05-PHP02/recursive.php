0972a20c132368c691039130824ba01a5635f4b9
nazwazhrr <nazwaazahra883@gmail.com>
2025-09-30 17:19:22 +0700

    Prak 5

--- JS05-PHP02/recursive.php
+++ JS05-PHP02/recursive.php
@@ -1,9 +1,5 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
-    }
-
-tampilkanHaloDunia();
+for ($i = 0; $i < 25; $i++) {
+    echo "Perulangan ke-{$i} <br>";
+}
 ?>
