7fdbf2a476894f1fa1ed0c84f05a2294a4fdd322
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-18 23:23:36 +0700

    Praktikum 5 No.2

--- Jobsheet_7/regex.php
+++ Jobsheet_7/regex.php
@@ -7,4 +7,12 @@
     } else {
         echo "Tidak ada huruf kecil!";
     }
+    $pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+
+    if(preg_match( $pattern, $text, $matches ) ) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
 ?>
