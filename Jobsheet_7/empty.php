83ceeecdefc61cf802b7303e35b53fb8a41d9dff
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-18 20:48:04 +0700

    Praktikum 2 No.2

--- Jobsheet_7/empty.php
+++ Jobsheet_7/empty.php
@@ -5,4 +5,9 @@
     } else {
         echo "Array terdefinisi dan tidak kosong.<br>";
     }
+      if(empty($nonExistentVar)){
+        echo "Variabel tidak terdefinisi atau kosong.<br>";
+    } else {
+        echo "Variabel terdefinisi dan tidak kosong.<br>";
+    }
 ?>
