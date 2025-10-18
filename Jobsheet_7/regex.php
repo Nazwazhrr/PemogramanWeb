e98464b0f7afdeb417af0915b2893fd8158d2b5c
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-18 23:37:59 +0700

    Praktikum 5 No.4

--- Jobsheet_7/regex.php
+++ Jobsheet_7/regex.php
@@ -21,4 +21,12 @@
     $text = 'I like apple pie.';
     $new_text = preg_replace($pattern, $replacement, $text);
     echo $new_text;
+
+    $pattern = '/go{1,2}d/';
+    $text = 'god is good.';
+    if(preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
 ?>
