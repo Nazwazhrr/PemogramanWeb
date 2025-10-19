85673c43824cd6d1a19c3784497265766c3cae7a
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-19 22:43:52 +0700

    Praktikum 5

--- Jobsheet_7/regex.php
+++ Jobsheet_7/regex.php
@@ -3,24 +3,24 @@
     $text = 'This is a Sample Text.';
 
     if(preg_match($pattern, $text)){
-        echo "Huruf kecil ditemukan!";
+        echo "Huruf kecil ditemukan!" . "<br>";
     } else {
-        echo "Tidak ada huruf kecil!";
+        echo "Tidak ada huruf kecil!" . "<br>";
     }
     $pattern = '/[0-9]+/';
     $text = 'There are 123 apples.';
 
     if(preg_match( $pattern, $text, $matches ) ) {
-        echo "Cocokkan: " . $matches[0];
+        echo "Cocokkan: " . $matches[0] . "<br>";
     } else {
-        echo "Tidak ada yang cocok!";
+        echo "Tidak ada yang cocok!" . "<br>";
     }
-    
+
     $pattern = '/apple/';
     $replacement = 'banana';
     $text = 'I like apple pie.';
     $new_text = preg_replace($pattern, $replacement, $text);
-    echo $new_text;
+    echo $new_text . "<br>";
 
     $pattern = '/go{1,2}d/';
     $text = 'god is good.';
