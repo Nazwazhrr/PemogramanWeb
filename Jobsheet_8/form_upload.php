932c519bc04c589cbbaad3ff38931b59f97c5b12
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-26 18:35:50 +0700

    Praktikum 1 No.4

--- Jobsheet_8/form_upload.php
+++ Jobsheet_8/form_upload.php
@@ -1,11 +1,26 @@
-<html>
-    <head>
-        <title>File Upload</title>
-    </head>
-    <body>
-        <form action="upload.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="myfile">
-            <input type="submit" name="submit">
-        </form>
-    </body>
-</html>
+<?php
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize)
+    {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah.<br>";
+
+            echo "<p>Pratinjau gambar:</p>";
+            echo "<img src='$targetfile' width='200' style='height:auto; border:1px solid #ccc; margin-top:10px;'>";
+        }
+        else{
+            echo "Gagal mengunggah file.";
+        }
+    }
+    else{
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+    }
+}
+?>
